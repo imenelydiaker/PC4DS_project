355b546e0498e5ec036d57bab8e8ddccde0da06f
Mouad LARIBIA <71670456+Laribia@users.noreply.github.com>
2020-10-16 12:14:44 +0200

    Add files via upload

--- src/insurance_risk.R
+++ src/insurance_risk.R
@@ -1,16 +1,19 @@
-## File : insurance_risk.R 
+## File : insurance_risk.R
 
 
 ## Part 1 ####
 
 ## F(x) = 1 - (beta/x)^alpha (= y)
-## F-1(x) => x = beta / (1 - y)^(1/alpha)
+## F-1(x) => x = [beta / (1 - y)^(1/alpha)] - beta
 
 ## simulate size of claims per year
-inv.f <- function(y, alpha, beta) (beta / (1 - y)^(1/alpha))
+inv.f <-
+  function(y, alpha, beta)
+    ((beta / (1 - y) ^ (1 / alpha)) - beta)
 
-simu.X <- function(n_samples){
-  alpha = 3; beta = 100000
+simu.X <- function(n_samples) {
+  alpha = 3
+  beta = 100000
   y = runif(n_samples, 0, 1)
   inv.f(y, alpha, beta)
 }
@@ -23,7 +26,9 @@ plot(density(X), type = 'hist')
 hist(X)
 
 library(EnvStats)
-pareto = rpareto(n = 100, location = 100000, shape = 3)
+pareto = rpareto(n = 100,
+                 location = 100000,
+                 shape = 3)
 plot(density(pareto))
 plot(density(pareto), type = 'hist')
 hist(pareto)
@@ -49,13 +54,17 @@ hist(pareto)
 # }
 
 ## assets at the end of 5 years
-z <- function(t){
-  premiums = 5500000; z_array = c(1000000)
-  for(i in 1:t) { # i : annÃ©e prÃ©cÃ©dente car les tableaux en R commencent Ã  1 et z_array est dÃ©jÃ  initialisÃ© Ã  z_0
+z <- function(t) {
+  premiums = 5500000
+  z_array = c(1000000)
+  for (i in 1:t) {
+    # i : année précédente car les tableaux en R commencent à 1 et z_array est déjà initialisé à z_0
     claims = simu.X(n_samples = 100)
     print(sum(claims))
-    if(z_array[i] > 0) z_t = max(z_array[i] + premiums - sum(claims), 0)
-    else z_t = 0
+    if (z_array[i] > 0)
+      z_t = max(z_array[i] + premiums - sum(claims), 0)
+    else
+      z_t = 0
     z_array = append(z_array, z_t)
   }
   z_array
@@ -70,15 +79,22 @@ plot(0:5, res, type = 'line')
 ## Part 3 ####
 
 # assets + profit
-z.profit <- function(t){
-  premiums = 5500000; z0 = 1e6; z.prev = 0; z_array = c(z_0)
-  for(i in 1:t) {
+z.profit <- function(t) {
+  premiums = 5500000
+  z0 = 1e6
+  z.prev = 0
+  z_array = c(z_0)
+  for (i in 1:t) {
     claims = simu.X(n_samples = 100)
     print(sum(claims))
-    if(z_array[i] > 0) z_t = max(z_array[i] + premiums - sum(claims), 0)
-    else z_t = 0
-    if(z_t > 1e6) z_array = append(z_array, z_t - 1e6)
-    else z_array = append(z_array, z_t)
+    if (z_array[i] > 0)
+      z_t = max(z_array[i] + premiums - sum(claims), 0)
+    else
+      z_t = 0
+    if (z_t > 1e6)
+      z_array = append(z_array, z_t - 1e6)
+    else
+      z_array = append(z_array, z_t)
   }
   z_array
 }
@@ -86,4 +102,3 @@ z.profit <- function(t){
 
 res <- z.proft(t = 5)
 print(res)
-
