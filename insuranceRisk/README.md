fc1e6b0087cc143ecdae0b57f32e71127b140e64
Imene Lydia Kerboua <imenelydiaker@users.noreply.github.com>
2020-10-20 02:35:16 +0200

    add insurance risk package

---
+++ insuranceRisk/README.md
@@ -0,0 +1,16 @@
+# Insurance Risk - PC4DS Project Part 2
+Solve student problem of section 22.4 of chapter 22
+
+#### -- Project Status: Finished
+
+## Project Intro/Objective
+This project is a R package that contains functions of simulation for the problem of insurance risk. It also contains a test file that you can run in the directory Test.
+
+### Technologies
+* R
+
+## Getting Started
+1. Download this repository and open the project : insuranceRisk.Rproj
+2. Run : library(devtools) (install it if required)
+3. Run : load_all()
+4. Run the script Test/test.R
--- insuranceRisk/Test/test.R
+++ insuranceRisk/Test/test.R
@@ -1,99 +1,81 @@
 ## File : test.R
 ## Description : * tests methods of insuranceRisk package
 ##              * answers question of Chap 22 Students projects section 22.4 Insurance risk
+## Chapter 22, section 22.4
 
-library(insuranceRisk)
+library(pkgInsuranceRisk)
+
+## USEFUL FUNCTIONS ####
+
+## Function to plot different simulations of assets during t years
+plot.assets <- function(n = 20, FUN, t = 5){
+  fortunes <- c(); go.bust <- list()
+  for(i in seq(n)){
+    asset <- FUN(t)
+    go.bust <- append(go.bust, asset$go.bust)
+    fortunes <- append(fortunes, asset)
+    plot(0:5, asset$assets, type = 'l', xlab = 'years', ylab = 'assets')
+    par(new = TRUE)
+  }
+  go.bust
+}
+
+## Function to calculate the probability of a company to go bust
+bust.proba <- function(bust.logical){
+  true <- Reduce("+", lapply(bust, sum))
+  total <- length(bust)
+  true / total
+}
 
 ## Part 1 ####
 
 X = simu_X(n_samples = 1000, alpha = 3, beta = 1e5)
 print(X)
 
-plot(density(X))
 plot(density(X), type = 'hist')
 hist(X)
+par(new = TRUE)
+plot(density(X))
 
 library(EnvStats)
 pareto = rpareto(n = 100,
                  location = 100000,
                  shape = 3)
-plot(density(pareto))
 plot(density(pareto), type = 'hist')
-par()
 hist(pareto)
+par(new = TRUE)
+plot(density(pareto))
 
 ## Part 2 ####
 
+## Test of z(t)
 res <- z(5)
 print(res)
 plot(0:5, res$assets, type = 'l', xlab = 'years', ylab = 'assets')
 
-
 ## Plot company assets over 5 years
-fortunes <- c(); go.bust <- list()
-for(i in seq(20)){
-  par(new = TRUE)
-  asset <- z(5)
-  go.bust <- append(go.bust, asset$go.bust)
-  fortunes <- append(assets, asset)
-  plot(0:5, asset$assets, type = 'l', xlab = 'years', ylab = 'assets')
-}
+bust <- plot.assets(n = 20, FUN = z, t = 5)
 
-true <- Reduce("+", lapply(go.bust, sum))
-print(true)
-total <- length(go.bust)
-print(total)
-print(paste("Probability of going bust :", true / total))
+## Probability of going bust
+print(paste("Probability of going bust :", bust.proba(bust)))
 
 ## Part 3 ####
 
+## Test of z_profit function
 res2 <- z_profit(t = 5)
 print(res2)
 plot(0:5, res2$assets, type = "l", xlab = 'years', ylab = 'assets')
 
 ## Plot company assets with profit over 5 years
-fortunes <- c(); go.bust <- list()
-for(i in seq(20)){
-  par(new = TRUE)
-  asset <- z_profit(5)
-  go.bust <- append(go.bust, asset$go.bust)
-  fortunes <- append(assets, asset)
-  plot(0:5, asset$assets, type = 'line', xlab = 'years', ylab = 'assets')
-}
-# print(assets)
+bust <- plot.assets(n = 20, FUN = z_profit, t = 5)
 
-true <- Reduce("+", lapply(go.bust, sum))
-print(true)
-total <- length(go.bust)
-print(total)
-print(paste("Probability of going bust :", true / total))
+## Probability of going bust
+print(paste("Probability of going bust :", bust.proba(bust)))
 
 
-# total profits taken during 5 years
+# Total profits taken over 5 years
 fortunes <- z_profit(t = 5)
 print(fortunes)
 plot(0:5, fortunes$assets, type = "l", xlab = 'years', ylab = 'assets')
 profits <- fortunes$profits
 profits
-
-
-## Extra Part ####
-
-## Optimized z function()
-resultat <- z_optimized(5, TRUE)
-print(resultat)
-plot(0:5, resultat$assets, type = "l", xlab = 'years', ylab = 'assets')
-
-fortunes <- c()
-for(i in seq(20)){
-  par(new = TRUE)
-  asset <- z_optimized(t = 5, profit = TRUE)
-  go.bust <- append(go.bust, asset$go.bust)
-  fortunes <- append(assets, asset)
-  plot(0:5, asset$assets, type = 'line', xlab = 'years', ylab = 'assets')
-}
-# print(assets)
-
-true <- Reduce("+", lapply(go.bust, sum))
-total <- length(go.bust)
-print(paste("Probability of going bust :", true / total))
